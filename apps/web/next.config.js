bb9e9bf6c4d52ae5b99933aa9e81e7a1773cac25
DkDeepak001 <Deepakeswar001@gmail.com>
2024-02-20 19:42:12 +0530

    feat: add validator and tailwind base package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-//
-// export default nextConfig;
-
 import { fileURLToPath } from "url";
 import _jiti from "jiti";
 
@@ -12,7 +5,6 @@ const jiti = _jiti(fileURLToPath(import.meta.url));
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 jiti("./src/env");
-jiti("@stockHub/auth/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -21,7 +13,6 @@ const config = {
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [
     "@stockHub/api",
-    "@stockHub/auth",
     "@stockHub/db",
     "@stockHub/validators",
   ],
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,20 +1,41 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+//
+
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+import baseConfig from "@stockHub/tailwind/web";
+
+export default {
+  // We need to append the path to the UI package to the content array so that
+  // those classes are included correctly.
+  content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
+  presets: [baseConfig],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
       },
     },
   },
-  plugins: [],
-};
-export default config;
+} satisfies Config;
+
