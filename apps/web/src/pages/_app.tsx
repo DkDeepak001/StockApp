d7e32ed1359c4c269d64df805fa9a45483e7ce61
DkDeepak001 <Deepakeswar001@gmail.com>
2024-02-24 08:13:15 +0530

    feat:trpc init for web

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,49 @@
-import "./globals.css";
-import type { AppProps } from "next/app";
+// import { AppRouter } from "@stockHub/api";
+// import { withTRPC } from "@trpc/next";
+// import { AppType } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// const MyApp: AppType = ({ Component, pageProps }) => {
+//   return <Component {...pageProps} />;
+// };
+
+// export default withTRPC<AppRouter>({
+//   config({ ctx }) {
+//     const url = process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}/api/trpc`
+//       : "http://localhost:3000/api/trpc";
+
+//     return {
+//       links: "",
+//       url,
+//       // Add any other properties required by WithTRPCConfig here
+//       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+//     };
+//   },
+//   ssr: true,
+// })(MyApp);
+
+import "./globals.css";
+import type { AppType } from "next/app";
+import { Poppins } from "next/font/google";
+import Head from "next/head";
+
+import { api } from "~/utils/api";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
+  return (
+    <div className={`${poppins.variable} font-Poppins`}>
+      <Head>
+        <title>Stock App</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default api.withTRPC(MyApp);
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { api } from "~/utils/api";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data, isLoading } = api.auth.getSession.useQuery();
+  console.log("data", data, isLoading);
   return (
     <div className="bg-black h-[100vh] -full flex justify-center items-center text-white">
       Hello from Next js
