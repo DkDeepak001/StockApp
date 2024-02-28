cd83240adf3e889f024ce1b0195b60d18d5cbe67
DkDeepak001 <Deepakeswar001@gmail.com>
2024-02-28 20:02:52 +0530

    feat: auth home screen

--- apps/app/src/app/(public)/authHome.tsx
+++ apps/app/src/app/(public)/authHome.tsx
@@ -1,7 +1,21 @@
+import { useRouter } from "expo-router";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "~/components/buttons/button";
 
 export default function AuthHome() {
+  const router = useRouter()
   return (
-    <SafeAreaView className="bg-black">
-    </SafeAreaView>)
+    <SafeAreaView className="bg-black flex flex-1  items-center justify-center">
+      <View className="w-screen items-center gap-y-3">
+        <Button variants="fill" onPress={() => router.push('/login')}>
+          <Text className="text-black font-bold text-lg">Login</Text>
+        </Button>
+        <Button variants="outline" onPress={() => router.push("/register")}>
+          <Text className="text-white font-bold text-lg">Register</Text>
+        </Button>
+
+      </View>
+    </SafeAreaView>
+  )
 }
