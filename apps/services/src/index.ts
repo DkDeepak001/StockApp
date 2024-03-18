218e617c355b6815053fe01e54a1a661ca16a353
DkDeepak001 <Deepakeswar001@gmail.com>
2024-03-18 16:55:52 +0530

    feat: parsed kakfa consumed message

--- apps/services/src/index.ts
+++ apps/services/src/index.ts
@@ -1,27 +1,63 @@
-import { Kafka } from "@upstash/kafka"
-
+import { Kafka } from "@upstash/kafka";
 const kafka = new Kafka({
   url: process.env.KAFKA_URL!,
   username: process.env.KAFKA_USERNAME!,
   password: process.env.KAFKA_PASSWORD!,
-})
+});
 
-const c = kafka.consumer()
+const c = kafka.consumer();
+
+const apiUrl = 'http://localhost:3000/api/hashtag'
 
 async function main() {
   try {
-
     const messages = await c.consume({
       consumerGroupId: "group_1",
       instanceId: "instance_1",
       topics: ["hashtag"],
       autoOffsetReset: "earliest",
-    })
-    console.log(messages)
+    });
+    console.log("Recived messages", messages)
+
+    let parsedResults: parsedResultType[] = [];
+    const mentionRegEx = /\(([-0-9a-fA-F]+)\)/g;
+
+    messages.forEach(message => {
+      const { postId, description } = JSON.parse(message.value) as messageType['data'];
+      const parsedText: string[] = [];
+      let match;
+      while ((match = mentionRegEx.exec(description)) !== null) {
+        parsedText.push(match[1]);
+      }
+      parsedResults.push({ postId, uuid: parsedText });
+    });
+
+    if (parsedResults.length >= 1) {
+      console.log("sending")
+      await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(parsedResults)
+      })
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
-main()
 
+// Call main function initially
+main();
+
+// Call main function every minute
+setInterval(main, 6000); // 60000 milliseconds = 1 minute
+
+type parsedResultType = {
+  postId: string,
+  uuid: string[]
+}
+type messageType = {
+  data: {
+    postId: string
+    description: string
+  }
+}
 
