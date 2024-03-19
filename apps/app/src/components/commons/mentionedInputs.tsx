6cb1c44adfd30aed430bb425b94ea7b350a8d86b
DkDeepak001 <Deepakeswar001@gmail.com>
2024-03-19 10:40:47 +0530

    feat: if no tag add new tag

--- apps/app/src/components/commons/mentionedInputs.tsx
+++ apps/app/src/components/commons/mentionedInputs.tsx
@@ -6,9 +6,11 @@ import {
 } from "react-native"
 import { MentionInputProps as MInputProps, MentionSuggestionsProps } from "react-native-controlled-mentions/dist/types"
 import { MentionInput as MInput } from "react-native-controlled-mentions"
-import { Dispatch, FC, SetStateAction } from "react"
+import { FC } from "react"
 import { ScrollView } from "react-native-gesture-handler"
 import { api } from "~/utils/api"
+import uuid from 'react-native-uuid'
+
 
 
 type RenderSuggestionsProps = MentionSuggestionsProps & {
@@ -20,6 +22,7 @@ const RenderSuggestions: FC<RenderSuggestionsProps> = ({ keyword, onSuggestionPr
   if (keyword == null) {
     return null;
   }
+  const context = api.useUtils()
   return (
     <ScrollView className={`bg-cyan-50 absolute  max-h-40 z-30 w-40 ml-4`}
       style={{
@@ -28,20 +31,35 @@ const RenderSuggestions: FC<RenderSuggestionsProps> = ({ keyword, onSuggestionPr
       nestedScrollEnabled={true}>
       {tags
         .filter(one => one.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()))
-        .map(one => (
-          <Pressable
-            key={one.id}
-            onPress={() => {
-              Keyboard.dismiss()
-              onSuggestionPress(one)
-            }}
+        .map(one => {
+          return (
+            <Pressable
+              key={one.id}
+              onPress={() => {
+                Keyboard.dismiss()
+                onSuggestionPress(one)
+              }}
 
-            style={{ padding: 12 }}
-          >
-            <Text>{one.name}</Text>
-          </Pressable>
-        ))
+              style={{ padding: 12 }}
+            >
+              <Text>{one.name}</Text>
+            </Pressable>
+          )
+        })
       }
+      {tags.filter(one => one.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())).length === 0 && (
+        <Pressable
+          onPress={() => {
+            context.hashTag.all.setData(undefined, (old) => {
+              //@ts-ignore
+              return [{ name: keyword, id: uuid.v4() }, ...old]
+            })
+          }}
+          style={{ padding: 12 }}
+        >
+          <Text>Add new tag?</Text>
+        </Pressable>
+      )}
     </ScrollView>
   );
 };
